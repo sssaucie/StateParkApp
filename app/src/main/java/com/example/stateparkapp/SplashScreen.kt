eed3a7114f7bf169f5717d5c498be5090f4e178c
Emily Thorne <53380331+sssaucie@users.noreply.github.com>
2021-03-23 11:30:57 -0600

    splash update
    Fixed bug with splash and main activity.

--- app/src/main/java/com/example/stateparkapp/SplashScreen.kt
+++ app/src/main/java/com/example/stateparkapp/SplashScreen.kt
@@ -2,13 +2,25 @@ package com.example.stateparkapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 
 class SplashScreen : AppCompatActivity()
 {
+
+    private var timeOut : Long = 3000
+
+    private fun loadSplashScreen() {
+        //TODO: why is Handler deprecated? How to update?
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, timeOut)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+        loadSplashScreen()
     }
 }
